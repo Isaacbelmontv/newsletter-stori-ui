24796b5edb19a9a1e493f7859ef2f6085987b0f1
Isaac belmont <isaacbelmontv@gmail.com>
2024-06-03 09:37:44 -0600

    feat: README instructions and newsletter redirect

--- src/app/presenter/pages/components/layouts/header/header.component.ts
+++ src/app/presenter/pages/components/layouts/header/header.component.ts
@@ -3,11 +3,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    RouterModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
